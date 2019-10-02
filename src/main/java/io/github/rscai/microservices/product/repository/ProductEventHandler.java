d86114dda35dccc9b4c9f593f022ee6dec43da1d
Ray Cai <ray.s.cai@icloud.com>
2019-10-02 21:52:32 +0800

    completed product API test

--- src/main/java/io/github/rscai/microservices/product/repository/ProductEventHandler.java
+++ src/main/java/io/github/rscai/microservices/product/repository/ProductEventHandler.java
@@ -1,5 +1,35 @@
 package io.github.rscai.microservices.product.repository;
 
+import io.github.rscai.microservices.product.model.Product;
+import java.util.Date;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler(Product.class)
 public class ProductEventHandler {
 
+  @Autowired
+  private ProductRepository entityRepository;
+
+  @HandleBeforeCreate
+  public void handleProductCreate(Product entity) {
+    if (entity.getCreatedAt() == null) {
+      entity.setCreatedAt(new Date());
+    }
+    entity.setUpdatedAt(new Date());
+  }
+
+  @HandleBeforeSave
+  public void handleProductBeforeSave(Product entity) {
+    final Optional<Product> existedEntity = entityRepository.findById(entity.getId());
+    if (existedEntity.isPresent()) {
+      entity.setCreatedAt(existedEntity.get().getCreatedAt());
+    }
+    entity.setUpdatedAt(new Date());
+  }
 }
--- src/test/java/io/github/rscai/microservices/product/controller/ProductTest.java
+++ src/test/java/io/github/rscai/microservices/product/controller/ProductTest.java
@@ -1,5 +1,312 @@
 package io.github.rscai.microservices.product.controller;
 
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.halLinks;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.rscai.microservices.product.ProductServiceApplication;
+import io.github.rscai.microservices.product.model.ProductImage;
+import io.github.rscai.microservices.product.model.ProductVariant;
+import io.github.rscai.microservices.product.repository.ProductImageRepository;
+import io.github.rscai.microservices.product.repository.ProductVariantRepository;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.stream.Stream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.hypermedia.HypermediaDocumentation;
+import org.springframework.restdocs.hypermedia.LinkDescriptor;
+import org.springframework.restdocs.hypermedia.LinksSnippet;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.payload.RequestFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ActiveProfiles({"test"})
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = ProductServiceApplication.class)
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class ProductTest {
+
+  private static final String ENDPOINT = "/products";
+
+  @Autowired
+  private MockMvc mvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+  @Autowired
+  private ProductImageRepository imageRepository;
+  @Autowired
+  private ProductVariantRepository variantRepository;
+
+  private String imageAId;
+  private String imageBId;
+  private String imageCId;
+
+  private String variantAId;
+  private String variantBId;
+  private String variantCId;
+
+  private static LinksSnippet links(LinkDescriptor... descriptors) {
+    return HypermediaDocumentation.links(halLinks(), linkWithRel("self").description("self link"),
+        linkWithRel("product").description("self link"),
+        linkWithRel("images").description("related images"),
+        linkWithRel("variants").description("related ProductVariants")).and(descriptors);
+  }
+
+  private static RequestFieldsSnippet requestFields(FieldDescriptor... descriptors) {
+    return PayloadDocumentation.requestFields(
+        fieldWithPath("title").type(JsonFieldType.STRING).description("product's title"),
+        fieldWithPath("tags").type(JsonFieldType.ARRAY).description("tags"),
+        fieldWithPath("createdAt").type("Date").description("create timestamp").optional()
+            .ignored(),
+        fieldWithPath("updatedAt").type("Date").description("last update timestamp").optional()
+            .ignored()).and(descriptors);
+  }
+
+  private static ResponseFieldsSnippet responseFields(FieldDescriptor... descriptors) {
+    return PayloadDocumentation.responseFields(
+        fieldWithPath("title").type(JsonFieldType.STRING).description("product's title"),
+        fieldWithPath("tags").type(JsonFieldType.ARRAY).description("tags"),
+        fieldWithPath("createdAt").type("Date").description("create timestamp"),
+        fieldWithPath("updatedAt").type("Date").description("last update timestamp"),
+        subsectionWithPath("_links").description("links to other resources")).and(descriptors);
+  }
+
+  @Before
+  public void setUp() {
+    ProductImage imageA = new ProductImage();
+    imageA.setSrc("https://aaa.bbb.ccc/ddd.png");
+    imageA.setCreatedAt(new Date());
+    imageA.setUpdatedAt(new Date());
+
+    imageAId = imageRepository.save(imageA).getId();
+
+    ProductImage imageB = new ProductImage();
+    imageB.setSrc("https://bbb.ccc.ddd/eee.png");
+    imageB.setCreatedAt(new Date());
+    imageB.setUpdatedAt(new Date());
+
+    imageBId = imageRepository.save(imageB).getId();
+
+    ProductImage imageC = new ProductImage();
+    imageC.setSrc("https://ccc.ddd.eee/fff.png");
+    imageC.setCreatedAt(new Date());
+    imageC.setUpdatedAt(new Date());
+
+    imageCId = imageRepository.save(imageC).getId();
+
+    ProductVariant variantA = new ProductVariant();
+    variantA.setInventoryItemId("inventoryItemA");
+    variantA.setImages(Arrays.asList(imageRepository.findById(imageAId).get()));
+    variantAId = variantRepository.save(variantA).getId();
+
+    ProductVariant variantB = new ProductVariant();
+    variantB.setInventoryItemId("inventoryItemB");
+    variantB.setImages(Arrays.asList(imageRepository.findById(imageBId).get()));
+    variantBId = variantRepository.save(variantB).getId();
+
+    ProductVariant variantC = new ProductVariant();
+    variantC.setInventoryItemId("inventoryItemC");
+    variantC.setImages(Arrays.asList(imageRepository.findById(imageCId).get()));
+    variantCId = variantRepository.save(variantC).getId();
+  }
+
+  @Test
+  public void testCreateAndGet() throws Exception {
+    final String imageALink = obtainLinkOfImage(imageAId);
+    final String imageBLink = obtainLinkOfImage(imageBId);
+
+    final String variantALink = obtainLinkOfVariant(variantAId);
+    final String variantBLink = obtainLinkOfVariant(variantBId);
+
+    final String title = "New Product";
+    final String ELECTRONICS = "Electronics";
+    final String MOBILE = "Mobile";
+
+    String createResponse = mvc.perform(
+        post(ENDPOINT).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+            .content(String.format(
+                "{\"title\":\"%s\",\"tags\":[\"%s\",\"%s\"],\"images\":[\"%s\",\"%s\"],\"variants\":[\"%s\",\"%s\"]}",
+                title, ELECTRONICS, MOBILE, imageALink, imageBLink, variantALink, variantBLink)))
+        .andDo(print()).andExpect(status().isCreated()).andExpect(jsonPath("$.title", is(title)))
+        .andExpect(jsonPath("$.createdAt", notNullValue()))
+        .andExpect(jsonPath("$.updatedAt", notNullValue()))
+        .andExpect(jsonPath("$._links.images", notNullValue()))
+        .andExpect(jsonPath("$._links.variants", notNullValue()))
+        .andDo(document("product/create", links(), requestFields(
+            fieldWithPath("images").type(JsonFieldType.ARRAY)
+                .description("links of referred ProductImage"),
+            fieldWithPath("variants").type(JsonFieldType.ARRAY)
+                .description("links of referred ProductVariant")), responseFields()))
+        .andReturn().getResponse().getContentAsString();
+
+    String productId = Stream
+        .of(objectMapper.readTree(createResponse).at("/_links/self/href").asText().split("/"))
+        .reduce((first, second) -> second).orElse(null);
+
+    mvc.perform(get(ENDPOINT + "/{id}", productId).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andExpect(jsonPath("$.title", is(title)))
+        .andExpect(jsonPath("$.createdAt", notNullValue()))
+        .andExpect(jsonPath("$.updatedAt", notNullValue()))
+        .andExpect(jsonPath("$._links.images", notNullValue()))
+        .andDo(document("product/getOne", links(),
+            pathParameters(parameterWithName("id").description("product's id")), responseFields()));
+
+    mvc.perform(get(ENDPOINT + "/{id}/images", productId).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andExpect(jsonPath("$._embedded.productImages", hasSize(2)))
+        .andDo(document("product/getImages",
+            pathParameters(parameterWithName("id").description("product's id"))));
+
+    mvc.perform(get(ENDPOINT + "/{id}/variants", productId).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andExpect(jsonPath("$._embedded.productVariants", hasSize(2)))
+        .andDo(document("product/getVariants",
+            pathParameters(parameterWithName("id").description("product's id"))));
+  }
+
+  @Test
+  public void testUpdate() throws Exception {
+    // preset product
+    final String imageALink = obtainLinkOfImage(imageAId);
+    final String imageBLink = obtainLinkOfImage(imageBId);
+
+    final String variantALink = obtainLinkOfVariant(variantAId);
+    final String variantBLink = obtainLinkOfVariant(variantBId);
+
+    final String title = "New Product";
+    final String ELECTRONICS = "Electronics";
+    final String MOBILE = "Mobile";
+
+    String createResponse = mvc.perform(
+        post(ENDPOINT).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+            .content(String.format(
+                "{\"title\":\"%s\",\"tags\":[\"%s\",\"%s\"],\"images\":[\"%s\",\"%s\"],\"variants\":[\"%s\",\"%s\"]}",
+                title, ELECTRONICS, MOBILE, imageALink, imageBLink, variantALink, variantBLink)))
+        .andExpect(status().isCreated())
+        .andReturn().getResponse().getContentAsString();
+
+    String productId = Stream
+        .of(objectMapper.readTree(createResponse).at("/_links/self/href").asText().split("/"))
+        .reduce((first, second) -> second).orElse(null);
+
+    final String imageCLink = obtainLinkOfImage(imageCId);
+    final String variantCLink = obtainLinkOfVariant(variantCId);
+
+    final String newTitle = "New Title";
+
+    mvc.perform(put(ENDPOINT + "/{id}", productId).accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON).content(String.format(
+            "{\"title\":\"%s\",\"tags\":[\"%s\"]}",
+            newTitle, MOBILE, imageBLink, imageCLink, variantBLink, variantCLink)))
+        .andExpect(status().isOk()).andDo(document("product/update", links(),
+        pathParameters(parameterWithName("id").description("product's id")), PayloadDocumentation
+            .requestFields(
+                fieldWithPath("title").type(JsonFieldType.STRING).description("product's title"),
+                fieldWithPath("tags").type(JsonFieldType.ARRAY).description("tag list")),
+        responseFields()));
+
+    mvc.perform(get(ENDPOINT + "/{id}", productId).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andExpect(jsonPath("$.title", is(newTitle)))
+        .andExpect(jsonPath("$.tags", hasSize(1))).andExpect(jsonPath("$.tags[0]", is(MOBILE)));
+
+    mvc.perform(put(ENDPOINT + "/{id}/images", productId)
+        .contentType(MediaType.parseMediaType("text/uri-list"))
+        .content(imageBLink + "\n" + imageCLink)).andExpect(status().isNoContent()).andDo(
+        document("product/updateImages",
+            pathParameters(parameterWithName("id").description("product's id"))));
+
+    mvc.perform(get(ENDPOINT + "/{id}/images", productId).accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk()).andExpect(jsonPath("$._embedded.productImages", hasSize(2)))
+        .andExpect(jsonPath("$._embedded.productImages[0]._links.self.href", endsWith(imageBId)))
+        .andExpect(jsonPath("$._embedded.productImages[1]._links.self.href", endsWith(imageCId)));
+
+    mvc.perform(put(ENDPOINT + "/{id}/variants", productId)
+        .contentType(MediaType.parseMediaType("text/uri-list"))
+        .content(variantBLink + "\n" + variantCLink)).andExpect(status().isNoContent()).andDo(
+        document("product/updateVariants",
+            pathParameters(parameterWithName("id").description("product's id"))));
+
+    mvc.perform(get(ENDPOINT + "/{id}/variants", productId).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andExpect(jsonPath("$._embedded.productVariants", hasSize(2)))
+        .andExpect(
+            jsonPath("$._embedded.productVariants[0]._links.self.href", endsWith(variantBId)))
+        .andExpect(
+            jsonPath("$._embedded.productVariants[1]._links.self.href", endsWith(variantCId)));
+  }
+
+  @Test
+  public void testDelete() throws Exception {
+    // preset product
+    final String imageALink = obtainLinkOfImage(imageAId);
+    final String imageBLink = obtainLinkOfImage(imageBId);
+
+    final String variantALink = obtainLinkOfVariant(variantAId);
+    final String variantBLink = obtainLinkOfVariant(variantBId);
+
+    final String title = "New Product";
+    final String ELECTRONICS = "Electronics";
+    final String MOBILE = "Mobile";
+
+    String createResponse = mvc.perform(
+        post(ENDPOINT).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+            .content(String.format(
+                "{\"title\":\"%s\",\"tags\":[\"%s\",\"%s\"],\"images\":[\"%s\",\"%s\"],\"variants\":[\"%s\",\"%s\"]}",
+                title, ELECTRONICS, MOBILE, imageALink, imageBLink, variantALink, variantBLink)))
+        .andExpect(status().isCreated())
+        .andReturn().getResponse().getContentAsString();
+
+    String productId = Stream
+        .of(objectMapper.readTree(createResponse).at("/_links/self/href").asText().split("/"))
+        .reduce((first, second) -> second).orElse(null);
+
+    mvc.perform(delete(ENDPOINT + "/{id}", productId)).andExpect(status().isNoContent()).andDo(
+        document("product/delete",
+            pathParameters(parameterWithName("id").description("product's id"))));
+
+    mvc.perform(get(ENDPOINT + "/{id}", productId)).andExpect(status().isNotFound());
+  }
+
+  private String obtainLinkOfImage(final String imageId) throws Exception {
+    String responseContent = mvc
+        .perform(get("/productImages/{id}", imageId).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+    return objectMapper.readTree(responseContent).at("/_links/self/href").asText();
+  }
+
+  private String obtainLinkOfVariant(final String variantId) throws Exception {
+    String responseContent = mvc
+        .perform(get("/productVariants/{id}", variantId).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+    return objectMapper.readTree(responseContent).at("/_links/self/href").asText();
+  }
 
 }
