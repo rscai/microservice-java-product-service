b9affffbdb762bbc80e6a86938c998c72438b957
Ray Cai <ray.s.cai@icloud.com>
2020-10-11 14:44:24 +0800

    Corrected security configuration

--- src/main/java/io/github/rscai/microservices/catalog/SecurityConfig.java
+++ src/main/java/io/github/rscai/microservices/catalog/SecurityConfig.java
@@ -1,15 +1,10 @@
 package io.github.rscai.microservices.catalog;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Profile("!test")
 @EnableWebSecurity
